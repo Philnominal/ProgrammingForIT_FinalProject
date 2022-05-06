63e6f70de36b05b00548c97cace32d2bf6cff9cc
Philnominal <103754311+Philnominal@users.noreply.github.com>
2022-05-06 15:30:23 -0400

    Update Final_Project.py

--- Final_Project.py
+++ Final_Project.py
@@ -1,8 +1,10 @@
 from tkinter import *
 from tkinter import ttk
 import random
+from mss import mss
 
-
+#Creates the function that generates random ability scores and converts those scores into
+#a dictionary.
 def attributeGen():
     STR = random.randint(8, 18)
     DEX = random.randint(8, 18)
@@ -10,13 +12,17 @@ def attributeGen():
     INT = random.randint(8, 18)
     WIS = random.randint(8, 18)
     CHA = random.randint(8, 18)
-    attributes = STR, DEX, CON, INT, WIS, CHA
-    return attributes
+    Stats = {"STR": STR,
+             "DEX": DEX,
+             "CON": CON,
+             "INT": INT,
+             "WIS": WIS,
+             "CHA": CHA}
+    return Stats
 
-b = attributeGen()
-print(b)
-print(b[2])
 
+#Activates the attribute generation function.
+Stats = attributeGen()
 
 #Settings for the initial window
 window = Tk()
@@ -64,7 +70,7 @@ lbl_STR = Label(window, text="Strength", relief ="raised", font=("Ariel Bold", 1
                 borderwidth="2", bg="#e6bc5c")
 lbl_STR.grid(column=4, row=1)
 #Creates the Label to show generated strength score
-lbl_STRscore = Label(window, text=b[0], relief ="sunken", font=("Ariel Bold", 14), width=4,
+lbl_STRscore = Label(window, text=Stats['STR'], relief ="sunken", font=("Ariel Bold", 14), width=4,
                 borderwidth="2", bg="white")
 lbl_STRscore.grid(column=5, row=1)
 
@@ -73,7 +79,7 @@ lbl_DEX = Label(window, text="Dexterity", relief ="raised", font=("Ariel Bold", 
                 borderwidth="2", bg="#e6bc5c")
 lbl_DEX.grid(column=4, row=2)
 #Creates the Label to show generated Dexterity score
-lbl_DEXscore = Label(window, text=b[1], relief ="sunken", font=("Ariel Bold", 14), width=4,
+lbl_DEXscore = Label(window, text=Stats['DEX'], relief ="sunken", font=("Ariel Bold", 14), width=4,
                 borderwidth="2", bg="white")
 lbl_DEXscore.grid(column=5, row=2)
 
@@ -82,7 +88,7 @@ lbl_CON = Label(window, text="Constitution", relief ="raised", font=("Ariel Bold
                 borderwidth="2", bg="#e6bc5c")
 lbl_CON.grid(column=4, row=3)
 #Creates the Label box to show generated Constitution score
-lbl_CONscore = Label(window, text=b[2], relief ="sunken", font=("Ariel Bold", 14), width=4,
+lbl_CONscore = Label(window, text=Stats['CON'], relief ="sunken", font=("Ariel Bold", 14), width=4,
                 borderwidth="2", bg="white")
 lbl_CONscore.grid(column=5, row=3)
 
@@ -91,7 +97,7 @@ lbl_INT = Label(window, text="Intelligence", relief ="raised", font=("Ariel Bold
                 borderwidth="2", bg="#e6bc5c")
 lbl_INT.grid(column=6, row=1)
 #Creates the Label box to show generated Intelligence score
-lbl_INTscore = Label(window, text=b[3], relief ="sunken", font=("Ariel Bold", 14), width=4,
+lbl_INTscore = Label(window, text=Stats['INT'], relief ="sunken", font=("Ariel Bold", 14), width=4,
                 borderwidth="2", bg="white")
 lbl_INTscore.grid(column=7, row=1)
 
@@ -100,7 +106,7 @@ lbl_WIS = Label(window, text="Wisdom", relief ="raised", font=("Ariel Bold", 14)
                 borderwidth="2", bg="#e6bc5c")
 lbl_WIS.grid(column=6, row=2)
 #Creates the Label box to show generated Wisdom score
-lbl_WISscore = Label(window, text=b[4], relief ="sunken", font=("Ariel Bold", 14), width=4,
+lbl_WISscore = Label(window, text=Stats['WIS'], relief ="sunken", font=("Ariel Bold", 14), width=4,
                 borderwidth="2", bg="white")
 lbl_WISscore.grid(column=7, row=2)
 
@@ -109,7 +115,7 @@ lbl_CHA = Label(window, text="Charisma", relief ="raised", font=("Ariel Bold", 1
                 borderwidth="2", bg="#e6bc5c")
 lbl_CHA.grid(column=6, row=3)
 #Creates the Label box to show generated Charisma score
-lbl_CHAscore = Label(window, text=b[5], relief ="sunken", font=("Ariel Bold", 14), width=4,
+lbl_CHAscore = Label(window, text=Stats['CHA'], relief ="sunken", font=("Ariel Bold", 14), width=4,
                 borderwidth="2", bg="white")
 lbl_CHAscore.grid(column=7, row=3)
 
@@ -117,29 +123,41 @@ lbl_CHAscore.grid(column=7, row=3)
 lbl_Level = Label(window, text="Level:", relief="raised", font=("Ariel Bold", 14), width=10,
                borderwidth="2", bg="#e6bc5c")
 lbl_Level.grid(column=0, row=5)
-#Creates the Level Dropdown box to choose level of character
-levelOption = ttk.Combobox(window, width=2)
+#Creates the Level box to show level of character
+levelOption = Label(window, text="1", relief="sunken", font=("Ariel Bold", 10), width=2,
+               borderwidth="2")
 levelOption.grid(column=1, row=5)
-levelOption['values']=(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-levelOption.current(0)
 
 #Creates the HP (Hit points) Label
 lbl_HP = Label(window, text="HP:", relief="raised", font=("Ariel Bold", 14), width=10,
                borderwidth="2", bg="#e6bc5c")
 lbl_HP.grid(column=0, row=6)
 #Creates the Label to display the Max HP of the Character
-lbl_HPscore = Label(window, text="PlaceH", relief ="sunken", font=("Ariel Bold", 12), width=6,
+lbl_HPscore = Label(window, text=10+Stats['CON']-12, relief ="sunken", font=("Ariel Bold", 12), width=6,
                 borderwidth="2", bg="white")
 lbl_HPscore.grid(column=1, row=6)
 
-#Creates the Save File Button
-saveFile_btn = Button(window, text="Save File", font=("Ariel Bold", 14), width=8,
-                      borderwidth="2", bg="#e6bc5c")
-saveFile_btn.grid(column=0, row=10)
+#Creates the function that gets the value of the nameTxt entry box and returns it.
+def get_text():
+    GetText = nameTxt.get()
+    return GetText
 
-#
-printFile_btn = Button(window, text="Print", font=("Ariel Bold", 14), width=8,
-                      borderwidth="2", bg="#e6bc5c")
+#Creates Screenshot and write to text dictionary file/Save as .png /.txt function.
+def screenshot(SaveName):
+    pngName = SaveName + "_Screenshot.png"
+    statsName = SaveName + "_Stats.txt"
+    with open(statsName, 'w') as StatsLog:
+        for line, value in Stats.items():
+            StatsLog.write('%s:%s\n' % (line, value))
+    with mss() as sct:
+        filename = sct.shot(output=pngName)
+        
+
+#Creates the Print/Save button and activates the "screenshot" function.
+#This both writes to a txt file and saves a screenshot as a PNG. The Save
+#File is saved as whatever is input for a name in the Name entry box.
+printFile_btn = Button(window, text="Print/Save", font=("Ariel Bold", 14), width=8,
+                      borderwidth="2", bg="#e6bc5c", command=lambda: [get_text(), screenshot(get_text())])
 printFile_btn.grid(column=0, row=11)
 
 window.mainloop()
